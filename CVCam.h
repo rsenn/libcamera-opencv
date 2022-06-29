55c3542573ae3f8e8928a4eacb6e10e1d9dad580
Roman Senn <roman.l.senn@gmail.com>
2022-06-29 02:00:08 +0200

    ...

---
+++ CVCam.h
@@ -0,0 +1,40 @@
+#pragma once
+
+
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <thread>
+
+#include <libcamera/libcamera.h>
+#include <opencv2/opencv.hpp>
+
+#include "mapped_framebuffer.h"
+
+#include "event_loop.h"
+
+#define TIMEOUT_SEC 3
+
+using namespace libcamera;
+
+class CVCam : public cv::VideoCapture
+{
+public:
+
+    static void processRequest(Request *request);
+    static void requestComplete(Request *request);
+    std::string cameraName(Camera *camera);
+    cv::String getBackendName() const { return "libcamera"; }
+
+    int start();
+    int go();
+    int finish();
+
+    std::shared_ptr<Camera> camera;
+    EventLoop loop;
+    std::unique_ptr<std::thread> aThread;
+    Stream *stream;
+    FrameBufferAllocator *allocator;
+    std::unique_ptr<CameraManager> cm;
+    std::vector<std::unique_ptr<Request>> requests;
+};
